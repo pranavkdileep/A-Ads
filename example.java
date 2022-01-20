d185d5b5cef2ec53aefb70f4c3912c82226cf228
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2022-01-20 11:22:29 +0530

    Update example.java

--- example.java
+++ example.java
@@ -4,7 +4,7 @@ View inflate = getLayoutInflater().inflate(R.layout.cust, null);
 
 dialog2.setView(inflate);
 
-dialog2.setTitle("Exit");
+dialog2.setTitle(Advertisement");
 
 WebView webview1 = (WebView) inflate.findViewById(R.id.webview1);
 		webview1.getSettings().setJavaScriptEnabled(true);
@@ -16,8 +16,7 @@ Button but2 = (Button) inflate.findViewById(R.id.button2);
 
 webview1.loadUrl("https://acceptable.a-ads.com/1901951");
 
-but1.setOnClickListener(new OnClickListener() { public void onClick(View view) {
-MainActivity.this.finish(); } });
+
 
 but2.setOnClickListener(new OnClickListener() { public void onClick(View view) { dialog2.dismiss(); } });
 
