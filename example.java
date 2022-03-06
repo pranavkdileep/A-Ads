05490aa0599bcb829ced4a4675a400c7c259202a
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2022-03-06 12:44:29 +0530

    Update example.java

--- example.java
+++ example.java
@@ -6,6 +6,18 @@
         android:gravity="right"
         android:orientation="vertical">
 
+<TextView
+            android:id="@+id/textView3"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_gravity="right"
+            android:layout_marginRight="10dp"
+            android:gravity="right"
+            android:text="Please Wait"
+            android:textSize="16sp"
+            android:textStyle="bold" />
+
+
         <Button
             android:id="@+id/button2"
             android:layout_width="100dp"
@@ -34,9 +46,18 @@
         webview1.getSettings().setSupportZoom(false);
 
         Button but2 = (Button) inflate.findViewById(R.id.button2);
-        webview1.setWebViewClient(new WebViewClient());
-        webview1.loadUrl("https://gotolink.techypranav.eu.org/");
+TextView textview1 = (TextView) inflate.findViewById(R.id.textView3);
+webview1.setWebViewClient(new WebViewClient());
+        webview1.loadUrl("https://acceptable.a-ads.com/1901951");
 
+
+but2.setVisibility(View.INVISIBLE);
+        but2.postDelayed(new Runnable() {
+            public void run() {
+                but2.setVisibility(View.VISIBLE);
+                textview1.setVisibility(View.INVISIBLE);
+            }
+        }, 3000);
 
 
         but2.setOnClickListener(new View.OnClickListener() { public void onClick(View view) { dialog2.dismiss(); } });
